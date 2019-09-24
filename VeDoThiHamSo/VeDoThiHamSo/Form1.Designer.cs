854d2d2ac9cbb0c8dd3d6c3dc2b29afd21157e2d
Phan Đình Hoàng <43243178+17110144@users.noreply.github.com>
2019-09-24 17:47:09 +0700

    create
    Tạo project và giao diện

---
+++ VeDoThiHamSo/VeDoThiHamSo/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace VeDoThiHamSo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.GraphFuntionMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.StraightLineMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.StraightParagraphMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.QuadraticFunctionMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.CubicFunctionMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.yaxbcxdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CircleMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ElipMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.HyperbolMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.AsinwxMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.AtanwxMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogaxMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExponentialFunctionMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.DrawingPropertiesMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.PenSizeMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.StyleMTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GraphFuntionMTS,
+            this.DrawingPropertiesMTS});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // GraphFuntionMTS
+            // 
+            this.GraphFuntionMTS.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StraightLineMTS,
+            this.StraightParagraphMTS,
+            this.QuadraticFunctionMTS,
+            this.CubicFunctionMTS,
+            this.yaxbcxdToolStripMenuItem,
+            this.CircleMTS,
+            this.ElipMTS,
+            this.HyperbolMTS,
+            this.AsinwxMTS,
+            this.AtanwxMTS,
+            this.LogaxMTS,
+            this.ExponentialFunctionMTS});
+            this.GraphFuntionMTS.Name = "GraphFuntionMTS";
+            this.GraphFuntionMTS.Size = new System.Drawing.Size(101, 20);
+            this.GraphFuntionMTS.Text = "Graph Function";
+            // 
+            // StraightLineMTS
+            // 
+            this.StraightLineMTS.Name = "StraightLineMTS";
+            this.StraightLineMTS.Size = new System.Drawing.Size(311, 22);
+            this.StraightLineMTS.Text = "Straight Line [Nhập điểm đầu và điểm cuối]";
+            // 
+            // StraightParagraphMTS
+            // 
+            this.StraightParagraphMTS.Name = "StraightParagraphMTS";
+            this.StraightParagraphMTS.Size = new System.Drawing.Size(311, 22);
+            this.StraightParagraphMTS.Text = "StraightParagraph";
+            // 
+            // QuadraticFunctionMTS
+            // 
+            this.QuadraticFunctionMTS.Name = "QuadraticFunctionMTS";
+            this.QuadraticFunctionMTS.Size = new System.Drawing.Size(311, 22);
+            this.QuadraticFunctionMTS.Text = "Quadratic Function y = ax^2 + bx + c ";
+            // 
+            // CubicFunctionMTS
+            // 
+            this.CubicFunctionMTS.Name = "CubicFunctionMTS";
+            this.CubicFunctionMTS.Size = new System.Drawing.Size(311, 22);
+            this.CubicFunctionMTS.Text = "Cubic Function y = ax^3 + bx^2 + cx + d";
+            // 
+            // yaxbcxdToolStripMenuItem
+            // 
+            this.yaxbcxdToolStripMenuItem.Name = "yaxbcxdToolStripMenuItem";
+            this.yaxbcxdToolStripMenuItem.Size = new System.Drawing.Size(311, 22);
+            this.yaxbcxdToolStripMenuItem.Text = "y = (ax+b)/(cx+d)";
+            // 
+            // CircleMTS
+            // 
+            this.CircleMTS.Name = "CircleMTS";
+            this.CircleMTS.Size = new System.Drawing.Size(311, 22);
+            this.CircleMTS.Text = "Circle [Nhập tâm và bán kính]";
+            // 
+            // ElipMTS
+            // 
+            this.ElipMTS.Name = "ElipMTS";
+            this.ElipMTS.Size = new System.Drawing.Size(311, 22);
+            this.ElipMTS.Text = "Elip (x^2/a^2) + (y^2/b^2) = 1 [Nhập a và b]";
+            // 
+            // HyperbolMTS
+            // 
+            this.HyperbolMTS.Name = "HyperbolMTS";
+            this.HyperbolMTS.Size = new System.Drawing.Size(311, 22);
+            this.HyperbolMTS.Text = "Hyperbol  (x^2)/a^2 - (y^2)/b^2 = 1";
+            // 
+            // AsinwxMTS
+            // 
+            this.AsinwxMTS.Name = "AsinwxMTS";
+            this.AsinwxMTS.Size = new System.Drawing.Size(311, 22);
+            this.AsinwxMTS.Text = "y = Asin(wx) [Nhập A, w]";
+            // 
+            // AtanwxMTS
+            // 
+            this.AtanwxMTS.Name = "AtanwxMTS";
+            this.AtanwxMTS.Size = new System.Drawing.Size(311, 22);
+            this.AtanwxMTS.Text = "y = Atan(wx)";
+            // 
+            // LogaxMTS
+            // 
+            this.LogaxMTS.Name = "LogaxMTS";
+            this.LogaxMTS.Size = new System.Drawing.Size(311, 22);
+            this.LogaxMTS.Text = "y = loga(x) [Nhập a, x]";
+            // 
+            // ExponentialFunctionMTS
+            // 
+            this.ExponentialFunctionMTS.Name = "ExponentialFunctionMTS";
+            this.ExponentialFunctionMTS.Size = new System.Drawing.Size(292, 22);
+            this.ExponentialFunctionMTS.Text = "Exponential Function y = a^x";
+            // 
+            // DrawingPropertiesMTS
+            // 
+            this.DrawingPropertiesMTS.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorMTS,
+            this.PenSizeMTS,
+            this.StyleMTS});
+            this.DrawingPropertiesMTS.Name = "DrawingPropertiesMTS";
+            this.DrawingPropertiesMTS.Size = new System.Drawing.Size(119, 20);
+            this.DrawingPropertiesMTS.Text = "Drawing Properties";
+            // 
+            // ColorMTS
+            // 
+            this.ColorMTS.Name = "ColorMTS";
+            this.ColorMTS.Size = new System.Drawing.Size(180, 22);
+            this.ColorMTS.Text = "Color";
+            // 
+            // PenSizeMTS
+            // 
+            this.PenSizeMTS.Name = "PenSizeMTS";
+            this.PenSizeMTS.Size = new System.Drawing.Size(180, 22);
+            this.PenSizeMTS.Text = "Size";
+            // 
+            // StyleMTS
+            // 
+            this.StyleMTS.Name = "StyleMTS";
+            this.StyleMTS.Size = new System.Drawing.Size(180, 22);
+            this.StyleMTS.Text = "Style";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Drawing Function Graphs";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem GraphFuntionMTS;
+        private System.Windows.Forms.ToolStripMenuItem StraightLineMTS;
+        private System.Windows.Forms.ToolStripMenuItem StraightParagraphMTS;
+        private System.Windows.Forms.ToolStripMenuItem QuadraticFunctionMTS;
+        private System.Windows.Forms.ToolStripMenuItem CubicFunctionMTS;
+        private System.Windows.Forms.ToolStripMenuItem yaxbcxdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CircleMTS;
+        private System.Windows.Forms.ToolStripMenuItem ElipMTS;
+        private System.Windows.Forms.ToolStripMenuItem HyperbolMTS;
+        private System.Windows.Forms.ToolStripMenuItem AsinwxMTS;
+        private System.Windows.Forms.ToolStripMenuItem AtanwxMTS;
+        private System.Windows.Forms.ToolStripMenuItem LogaxMTS;
+        private System.Windows.Forms.ToolStripMenuItem ExponentialFunctionMTS;
+        private System.Windows.Forms.ToolStripMenuItem DrawingPropertiesMTS;
+        private System.Windows.Forms.ToolStripMenuItem ColorMTS;
+        private System.Windows.Forms.ToolStripMenuItem PenSizeMTS;
+        private System.Windows.Forms.ToolStripMenuItem StyleMTS;
+    }
+}
+
