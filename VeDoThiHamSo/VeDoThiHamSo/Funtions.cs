4c5ce9b6ee97eb133a739eb0236b2c3916bc4f87
Phan Đình Hoàng <43243178+17110144@users.noreply.github.com>
2019-10-06 07:48:29 +0700

    Cập nhật class hàm số
    thêm các constructor thêm delegate để nhận dữ liệu từ form chính

--- VeDoThiHamSo/VeDoThiHamSo/Funtions.cs
+++ VeDoThiHamSo/VeDoThiHamSo/Funtions.cs
@@ -12,14 +12,49 @@ namespace VeDoThiHamSo
         public double b;
         public double c;
         public double d;
+        public double w;
+
+        public int mess;
+        public delegate void SendMessage(string Message);
+        public SendMessage Sender;
         public Funtions()
         {
             a = 0;
             b = 0;
             c = 0;
             d = 0;
+            w = 0;
+            Sender = new SendMessage(GetMessage);
+
         }
+        private void GetMessage(string Message)
+        {
+            this.mess = Convert.ToInt32(Message);
+        }
+        public Funtions(double a1)
+        {
+            a = a1;
+        }
+        public Funtions(double a1, double w1)
+        {
+            a = a1;
+            w = w1;
+        }
+        public Funtions(double a1, double b1, double c1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+        }
+
         public Funtions(double a1, double b1, double c1, double d1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+            d = d1;
+        }
+        public void InPut(double a1, double b1, double c1, double d1)
         {
             a = a1;
             b = b1;
@@ -28,11 +63,59 @@ namespace VeDoThiHamSo
         }
         public double f(double x)
         {
-            return (a * x + b);
-            //return (a * (Math.Pow(x, 2)) + b * x + c);
-            //return (a * (Math.Pow(x, 3)) + b * (Math.Pow(x, 2)) + c * x + d);
-            //return ((a * x + b) / (c * x + d));
+            double fx = -1;
+            switch (mess)
+            {
+                case 2:
+                    {
+                        fx = (a * (Math.Pow(x, 2)) + b * x + c);
+                    }
+                    break;
+                case 3:
+                    {
+                        fx = (a * (Math.Pow(x, 3)) + b * (Math.Pow(x, 2)) + c * x + d);
+                    }
+                    break;
+                case 4:
+                    {
+                        fx = ((a * x + b) / (c * x + d));
+                    }
+                    break;
+                case 6:
+                    {
 
+                    }
+                    break;
+                case 7:
+                    {
+                        fx = (a / x);
+                    }
+                    break;
+                case 8:
+                    {
+                        fx = (a * (Math.Sin(b * x)));
+                    }
+                    break;
+                case 9:
+                    {
+                        fx = (a * (Math.Tan(b * x)));
+                    }
+                    break;
+                case 10:
+                    {
+                        fx = (Math.Log(x, a));
+                    }
+                    break;
+                case 11:
+                    {
+                        fx = (Math.Pow(a, x));
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return fx;
         }
     }
 }
